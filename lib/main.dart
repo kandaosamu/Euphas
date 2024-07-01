2593870e4867ae5bc136396c51136f32da16d1fe
Pan Yu Wen <f94106185@gs.ncku.edu.tw>
2024-07-01 16:50:15 +0800

    register, get username, update Login() UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:euphas/pages/auth.dart';
-import 'package:euphas/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
